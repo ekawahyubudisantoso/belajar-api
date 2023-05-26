25f6e487b0664b96ded1e6e248bdf608ecb3d553
ekawahyubudisantoso <ekawahyubudisantoso22@gmail.com>
2023-05-26 07:47:27 +0700

    Belajar API CRUD (Create/membuat Postingan)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Resources\PostDetailResource;
 
 class PostController extends Controller
@@ -12,11 +13,26 @@ class PostController extends Controller
     public function index(){
         $posts = Post::all();
         // return response()->json(['data' => $posts]);
-        return PostResource::collection($posts);
+        return PostDetailResource::collection($posts->loadMissing('writer:id,username'));
     }
 
     public function show($id){
         $post = Post::with('writer:id,username')->findOrFail($id);
         return new PostDetailResource($post);
+    }
+
+    public function store(Request $request){
+        $validated = $request->validate([
+            'title'         => 'required|max:255',
+            'news_content'  => 'required',
+        ]);
+
+        $request['author'] = Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
+    }
+
+    public function update(Request $request, $id){
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,15 @@ use App\Http\Controllers\AuthenticationController;
 //     return $request->user();
 // });
 
-Route::get('/posts', [PostController::class, 'index'])->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/me', [AuthenticationController::class, 'me']);
+    Route::get('/logout', [AuthenticationController::class, 'logout']);
+
+    Route::post('/posts', [PostController::class, 'store']);
+    Route::patch('/posts/{id}', [PostController::class, 'update']);
+});
+
+Route::get('/posts', [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
 
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/me', [AuthenticationController::class, 'me'])->middleware('auth:sanctum');
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
