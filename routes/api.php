fe4fd34b1d14a0cbb8be68c299ec429a29b3160f
ekawahyubudisantoso <ekawahyubudisantoso22@gmail.com>
2023-05-27 07:41:44 +0700

    Belajar CRUD (Delete/Menghapus psotingan)

--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,7 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('/posts', [PostController::class, 'store']);
     Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware('pemilik-postingan');
+    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->middleware('pemilik-postingan');
 });
 
 Route::get('/posts', [PostController::class, 'index']);
